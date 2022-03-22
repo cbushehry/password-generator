a4df3b302f59323196648ca318269da56ef58835
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2022-03-21 22:56:15 -0700

    fixed global var v2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,15 @@
-// Global var
-var arraySplit = ""; 
-var exitApplication = false; 
-var userInput = ""; 
-var finalPassword = ""; 
-var displayPassword = ""; 
-
-var pCriteria = {
-  lowercase: { indexNumber: 0 , criteria: "abcdefghijklmnopqrstuvwxyz"
-  },
-  uppercase: { indexNumber: 1, criteria: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  },
-  numeric: { indexNumber: 2, criteria: "0123456789"
-  },
-  symbols: { indexNumber: 3, criteria: "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
-  }
-}
-
-
-
-//function to generate password criteria
-var generatePassword = function() {
-
-  arraySplit = "";
-  exitApplication = false;
-  userInput = ""; 
-  finalPassword = ""; 
-  displayPassword = ""; 
-
-  window.alert("The criteria for password generator are lowercase, uppercase, numeric, and/or special characters.");
-
-
-
-
-
-
-
-
-}
-
-
+ // Global var
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var numbers = '0123456789';
+var symbols = '!#$%&*+/:;<=>?@^';
+var passwordLength = 0;
+var passwordPool = '';
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -52,6 +19,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
